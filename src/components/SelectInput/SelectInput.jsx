1b8628100d100e3d88b2b1d02ed6397726efb9b4
Denis <olga.volotovich@mail.ru>
2024-02-05 16:00:55 +0100

    fix: delete log from SelectInput

--- src/components/SelectInput/SelectInput.jsx
+++ src/components/SelectInput/SelectInput.jsx
@@ -3,7 +3,6 @@ export const SelectInput = ({ text, id, options }) => {
     const optionItems = options ? options.map((item, index) => (
         <option key={index}>{item}</option>
     )) : null;
-    console.log(optionItems);
 
     return (
         <>
